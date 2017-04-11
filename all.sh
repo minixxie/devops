4f2dd2fd0d5dd51666ab8536ae2a5c681d2e4792
Simon Xie <simon.xie@kuulabu.com>
2017-04-11 23:55:00 +0800

    draft

--- all.sh
+++ all.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
-golangImage="minixxie/golang:1.7"
+scriptPath=$(cd $(dirname $0); pwd)
+ls "$scriptPath"/*.go > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+	GOLANG=1
+fi
+ls "$scriptPath"/*.js > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+	JS=1
+fi
 
+if [ "$GOLANG" == 1 ]
+then
+
+golangImage="minixxie/golang:1.7"
+dockerRunGolang="docker run -it --rm --net ldev-backend -w /go/src/app -v \"\$PWD\":/go/src/app $golangImage "
 choice=$(dialog --menu "Choose Action" \
     28 100 40 \
     \
-    "echo -n 'pkgUrl: ' && read pkgUrl && docker run --rm -w /go/src/app -v \"\$PWD\":/go/src/app $golangImage bash -c 'govendor add $pkgUrl'" \
-        "govendor add <gopkg_uri>                                              " \
-    "docker-compose -f ldev-docker-compose.yml rm -f" \
+    "docker ps -a" \
+    "docker process list                                                                                         " \
+    "#=== server ===#"          "#=== server ===" \
+    "docker-compose -f ldev-docker-compose.yml stop && docker-compose -f ldev-docker-compose.yml rm -f" \
         "server: stop                                              " \
     "docker-compose -f ldev-docker-compose.yml build && docker-compose -f ldev-docker-compose.yml up -d && docker-compose -f ldev-docker-compose.yml logs -f" \
         "server: start                                              " \
+    "docker-compose -f ldev-docker-compose.yml stop && docker-compose -f ldev-docker-compose.yml rm -f  &&  docker-compose -f ldev-docker-compose.yml build && docker-compose -f ldev-docker-compose.yml up -d && docker-compose -f ldev-docker-compose.yml logs -f" \
+        "server: stop & start                                              " \
+    "#=== GOLANG ===#"          "#=== GOLANG ===" \
+    "$dockerRunGolang bash -c \"govendor init\"" \
+        "govendor init" \
+    "echo -n 'pkgUrl: ';read pkgUrl;$dockerRunGolang bash -c \"govendor fetch \$pkgUrl\"" \
+        "govendor fetch <gopkg_uri>                                              " \
+    "docker run --rm --net ldev-backend -w /go/src/app -v \"\$PWD\":/go/src/app $golangImage bash -c \"cd test && go test\"" \
+        "go test" \
+    "docker run -it --rm --net ldev-backend -w /go/src/app -v \"\$PWD\":/go/src/app $golangImage bash" \
+        "go container" \
 3>&1 2>&3 > $(tty))
 
+elif [ "$JS" == 1 ]
+then
+
+nodejsImage="node:7.8.0-alpine"
+choice=$(dialog --menu "Choose Action" \
+    28 100 40 \
+    \
+    "docker ps -a" \
+    "docker process list                                                                                         " \
+    "#=== server ===#"          "#=== server ===" \
+    "docker-compose -f ldev-docker-compose.yml stop && docker-compose -f ldev-docker-compose.yml rm -f" \
+        "server: stop                                              " \
+    "docker-compose -f ldev-docker-compose.yml build && docker-compose -f ldev-docker-compose.yml up -d && docker-compose -f ldev-docker-compose.yml logs -f" \
+        "server: start                                              " \
+    "docker-compose -f ldev-docker-compose.yml stop && docker-compose -f ldev-docker-compose.yml rm -f  &&  docker-compose -f ldev-docker-compose.yml build && docker-compose -f ldev-docker-compose.yml up -d && docker-compose -f ldev-docker-compose.yml logs -f" \
+        "server: stop & start                                              " \
+    "#=== JS ===#"          "#=== JS ===" \
+    "docker run --rm --net ldev-backend -w /usr/src/app -v \"\$PWD\":/usr/src/app $nodejsImage bash -c \"npm test\"" \
+        "nodejs test" \
+    "docker run -it --rm --net ldev-backend -w /usr/src/app -v \"\$PWD\":/usr/src/app $nodejsImage bash" \
+        "nodejs container" \
+3>&1 2>&3 > $(tty))
+
+fi
+
+dockerMachine=$(which docker-machine)
+if [ "$dockerMachine" != "" ]
+then
+    eval $(docker-machine env)
+fi
 exec bash -c "$choice"
